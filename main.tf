03fb713d71a1cfcbb2682412dcbc2cf6790e490c
theonlyharsh <creatorzuni@gmail.com>
2025-03-05 20:22:38 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+    "name" = "terraformvpc"
+    }
+  }
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      "name" = "terraformsubnet"
+    }
+}
+resource "aws_internet_gateway" "name" {
+    vpc_id = aws_vpc.name.id
+    tags = {
+      "name" = "terraformigw"
+    } 
+}
+resource "aws_instance" "name" {
+    ami = "ami-0a89fa9a6d8c7ad98"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.name.id
+    tags = {
+      "name" = "terraforminstance"
+    }
+}
